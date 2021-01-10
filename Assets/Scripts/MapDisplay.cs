d9485da03c2f5a95ee891134985c064983f69c8d
Haruhi_Aisaka <bobingbobs@protonmail.com>
2021-01-10 00:30:33 -1000

    Starting Code

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public GameObject tileMap;
+    public Tile tile;
+
+    private Tile[] tiles;
+
+    public void DrawNoiseMap(float[,] noiseMap, MapGenerator.DrawMode drawMode) 
+    {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colorMap[y * height + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+
+        if (drawMode == MapGenerator.DrawMode.noiseTexture)
+        {
+            texture.SetPixels(colorMap);
+            texture.Apply();
+
+            textureRenderer.sharedMaterial.mainTexture = texture;
+            textureRenderer.transform.localScale = new Vector3(width, 1, height);
+        }
+        else if (drawMode == MapGenerator.DrawMode.noiseTiles)
+        {
+            InstantiateTilesByColor(width, height, colorMap);
+        }
+
+    }
+
+    private void InstantiateTilesByColor(int width, int height, Color[] colorMap)
+    {
+        tiles = new Tile[width * height];
+        GameObject newTileMap = Instantiate(tileMap, this.transform);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Tile newTile = InstantiateTile(x, y, newTileMap);
+                newTile.GetComponent<SpriteRenderer>().color = colorMap[y * height + x];
+                tiles[y * height + x] = newTile;
+            }
+        }
+    }
+
+    
+    public void DrawTileMap(Sprite[,] spriteMap)
+    {
+        int width = spriteMap.GetLength(0);
+        int height = spriteMap.GetLength(1);
+
+        tiles = new Tile[width * height];
+        GameObject newTileMap = Instantiate(tileMap, this.transform);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Tile newTile = InstantiateTile(x, y, newTileMap);
+                newTile.GetComponent<SpriteRenderer>().sprite = spriteMap[x, y];
+                tiles[y * height + x] = newTile;
+            }
+        }
+    }
+
+    private Tile InstantiateTile(int x, int y, GameObject tileMap)
+    {
+        Tile newTile = Instantiate(tile, tileMap.transform);
+        newTile.transform.localPosition = new Vector3(x, y, 0);
+        return newTile;
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode {noiseTexture, noiseTiles, TerrainTiles}
+
+    public DrawMode drawMode;
+
+    public MapDisplay display;
+    public int mapWidth;
+
+    public int mapHeight;
+
+    public float noiseScale;
+
+    public int seed;
+
+    [Range (0,10)]
+    public int octaves;
+
+    [Range(0,1)]
+    public float persistance;
+
+    public float lacunarity;
+
+    public Vector2 offset;
+
+    public TerrainTile[] terrainTiles;
+    public bool autoUpdate = false;
+    public void GenerateMap() 
+    {
+        float[,] noiseMap = 
+            Noise.GenerateNoise(
+                mapWidth, 
+                mapHeight, 
+                seed, 
+                noiseScale,
+                octaves,
+                persistance,
+                lacunarity,
+                offset);
+
+        if (drawMode == DrawMode.TerrainTiles)
+        {
+            Sprite[,] spriteMap = SpriteMapFromNoiseMap(noiseMap);
+            display.DrawTileMap(spriteMap);
+        }
+            
+        else 
+            display.DrawNoiseMap(noiseMap, drawMode);
+    }
+
+    private Sprite[,] SpriteMapFromNoiseMap(float[,] noiseMap)
+    {   
+        Sprite[,] spriteMap = new Sprite[mapWidth, mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                foreach (TerrainTile terrainTile in terrainTiles)
+                {
+                    if (noiseMap [x, y] >= terrainTile.minHeight)
+                        spriteMap[x, y] = terrainTile.sprite;
+                }
+            }
+        }
+
+        return spriteMap;
+    }
+
+    private void OnValidate() 
+    {
+        if (octaves < 0)
+            octaves = 0;
+        if (lacunarity < 0)
+            lacunarity = 0;
+        if (noiseScale < 0)
+            noiseScale = 0;
+    }
+
+    [System.Serializable]
+    public struct TerrainTile
+    {
+        public Sprite sprite;
+
+        [Range(0,1)]
+        public float minHeight;
+    }
+}
